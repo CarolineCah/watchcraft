be6f8e269321c8984a64020ed15970a45ca9abd4
Caroline Cah <61294176+CarolineCah@users.noreply.github.com>
2024-01-17 15:47:06 +0100

    Initial Commit

---
+++ WatchCraft Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchCraft Watch App
+//
+//  Created by Caroline Cah on 2024-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchCraft Watch App/WatchCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchCraftApp.swift
+//  WatchCraft Watch App
+//
+//  Created by Caroline Cah on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct WatchCraft_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
